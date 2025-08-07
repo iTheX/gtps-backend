7b5a79ac9c9ebfdb2ab648905283519363c20a45
ReallBoyy <rahmanbariq@gmail.com>
2025-08-07 04:47:23 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const rateLimiter = require('express-rate-limit');
+const compression = require('compression');
+
+app.use(compression({
+    level: 5,
+    threshold: 0,
+    filter: (req, res) => {
+        if (req.headers['x-no-compression']) {
+            return false;
+        }
+        return compression.filter(req, res);
+    }
+}));
+
+app.set('view engine', 'ejs');
+
+app.set('trust proxy', 1);
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept',
+    );
+    console.log(`[${new Date().toLocaleString()}] ${req.method} ${req.url} - ${res.statusCode}`);
+    next();
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+app.use(rateLimiter({ windowMs: 15 * 60 * 1000, max: 10000, headers: true }));
+
+app.all('/player/login/dashboard', function (req, res) {
+    res.render(__dirname + '/public/html/dashboard.ejs');
+});
+
+app.all('/player/growid/login/validate', (req, res) => {
+    const { _token, growId, pass, action } = req.body;
+    const token = (action === 'login' ? JSON.stringify({ server_name: _token.toUpperCase(), growId: growId, password: pass }) : JSON.stringify({ server_name: _token, growId: "", password: "" }));
+    res.send(
+        `{"status":"success","message":"Account Validated.","token":"${token}","url":"","accountType":"growtopia", "accountAge": 2}`,
+    );
+});
+
+app.all('/player/growid/checktoken', (req, res) => {
+    const { refreshToken } = req.body;
+    res.json({
+        status: 'success',
+        message: 'Account Validated.',
+        token: refreshToken,
+        url: '',
+        accountType: 'growtopia',
+        accountAge: 2
+    });
+});
+
+app.get('/', function (req, res) {
+   res.send('Hello Memek');
+});
+
+app.listen(5000, function () {
+    console.log('Listening on port 5000');
+});
