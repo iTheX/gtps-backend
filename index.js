7e4d5d85990b1c27e3b648ed9ae3cacdd2476e33
iTheX <86515403+iTheX@users.noreply.github.com>
2025-08-10 19:09:27 -0700

    Update index.js

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ app.all('/player/growid/checktoken', (req, res) => {
 });
 
 app.get('/', function (req, res) {
-   res.render(__dirname + '/public/html/dashboard.ejs');
+   res.send('Hello World');
 });
 
 app.listen(5000, function () {
